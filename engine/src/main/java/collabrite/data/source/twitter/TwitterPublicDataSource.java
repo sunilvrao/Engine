056b2562019a85df6beb3fff9c97da1518b09edd
aniltrust <anil@seamlesstrust.com>
2012-03-12 22:33:36 -0500

    twitter query code to search

---
+++ engine/src/main/java/collabrite/data/source/twitter/TwitterPublicDataSource.java
@@ -0,0 +1,34 @@
+package collabrite.data.source.twitter;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+/**
+ * A data source for twitter public feeds
+ * @author anil
+ */
+public class TwitterPublicDataSource {
+
+    /**
+     * <p>
+     * Given a string and a language, this method returns
+     * tweets from the public stream.
+     * </p>
+     * <p>
+     * The {@link QueryResult} will have information such as
+     * results per page, max tweet id etc.
+     * </p>
+     * @param queryStr any string to search (such as Yonex)
+     * @param lang 
+     * @return
+     * @throws TwitterException
+     */
+    public QueryResult query(String queryStr, String lang) throws TwitterException {
+        Twitter twitter = new TwitterFactory().getInstance();
+        Query query = new Query(queryStr);
+        query.setLang(lang);
+        return twitter.search(query);
+    }
+}
---
+++ engine/src/test/java/collabrite/test/data/sources/twitter/TwitterPublicDataSourceTestCase.java
@@ -0,0 +1,32 @@
+package collabrite.test.data.sources.twitter;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import twitter4j.QueryResult;
+import twitter4j.Tweet;
+import collabrite.data.source.twitter.TwitterPublicDataSource;
+
+/**
+ * Unit test the {@code TwitterPublicDataSource} class
+ * @author anil
+ */
+public class TwitterPublicDataSourceTestCase {
+    @Test
+    public void query() throws Exception{
+        System.setProperty("twitter4j.debug", "true");
+        TwitterPublicDataSource ds = new TwitterPublicDataSource();
+        QueryResult result = ds.query("yonex", "en");
+        display(result);
+        List<Tweet> tweets = result.getTweets();
+        System.out.println("Number of tweet="+tweets.size());
+        for (Tweet tweet : result.getTweets()) {
+            System.out.println(tweet.getFromUser() + ":" + tweet.getText());
+        }
+    }
+    
+    private void display(QueryResult result){
+        System.out.println("***Query Result=" + result);
+    }
+}
