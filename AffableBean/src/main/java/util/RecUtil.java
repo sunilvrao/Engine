c1c85fed10902ab07a9e86b061526144621fcfa7
aniltrust <anil@seamlesstrust.com>
2012-08-27 23:37:55 -0500

    updated path

--- AffableBean/src/main/java/util/RecUtil.java
+++ AffableBean/src/main/java/util/RecUtil.java
@@ -2,13 +2,15 @@ package util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.Properties;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
 
 import entity.Product;
 
@@ -58,31 +60,48 @@ public class RecUtil {
     public String getTenantid() {
         return tenantid;
     }
-     
+
     public void postBuy(Product product, String userID, String sessionID) {
         // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
-        HttpGet httpget = new HttpGet(url);
-        HttpParams params = httpget.getParams();
+         
 
         try {
-            params.setParameter("tenantid", tenantid);
-            params.setParameter("itemid", product.getId() + "");
-            params.setParameter("itemdescription", product.getDescription());
-            params.setParameter("itemurl", "/some.url");
-            params.setParameter("apiKey", apiKey);
-            params.setParameter("userid", userID);
-            params.setParameter("sessionid", sessionID);
-            params.setParameter("itemtype", "ITEM");
+            StringBuilder sb = new StringBuilder("?");
+            sb.append("tenantid").append("=").append(encode(tenantid)).append("&");
+            sb.append("itemid").append("=").append(encode(product.getId() + "")).append("&");
+            sb.append("itemdescription").append("=").append(encode(product.getDescription())).append("&");
+            sb.append("itemurl").append("=").append(encode("/some.url")).append("&");
+            sb.append("apikey").append("=").append( encode(apiKey)).append("&");
+            sb.append("userid").append("=").append( encode(userID)).append("&");
+            sb.append("sessionid").append("=").append( encode(sessionID)).append("&");
+            sb.append("itemtype").append("=").append( "ITEM");
             
+            String safeUrl = url + sb.toString();
+
+            HttpGet httpget = new HttpGet(safeUrl);
+            
+            
+            System.out.println("Making calls:"+httpget.getURI());
+
             // Execute HTTP get Request
             HttpResponse response = httpclient.execute(httpget);
             int statusCode = response.getStatusLine().getStatusCode();
             if(statusCode != 200){
                 throw new RuntimeException("Status Code=" + statusCode);
             }
+
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                System.out.println(EntityUtils.toString(entity));
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     } 
+    
+    @SuppressWarnings("deprecation")
+    private String encode(String str){
+        return URLEncoder.encode(str);
+    }
 }
